1004cfc054d72707a7c25456b17114b44c918516
Yahia Salem <yahiah.salem@gmail.com>
2020-06-07 17:04:47 +0200

    Adding ServiceProvider layout

--- app/src/main/java/com/example/pointme/activities/ServiceProviderMainActivity.java
+++ app/src/main/java/com/example/pointme/activities/ServiceProviderMainActivity.java
@@ -1,78 +1,141 @@
 package com.example.pointme.activities;
 
 import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
+import androidx.annotation.NonNull;
+
+import com.example.pointme.fragments.ProviderCalendarView;
+import com.example.pointme.fragments.ProvidersProfileFragment;
+import com.example.pointme.models.ServiceProvider;
+import com.example.pointme.utils.ConnectionLiveData;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ToggleButton;
 
 import com.example.pointme.R;
-import com.example.pointme.fragments.NewEventFragment;
-import com.example.pointme.fragments.ProvidersEventsFragment;
-import com.example.pointme.fragments.ScheduleFragment;
+import com.example.pointme.fragments.FavoritesFragment;
+import com.example.pointme.fragments.MyBookingFragment;
+import com.example.pointme.fragments.SettingsFragment;
+import com.example.pointme.models.Appointment;
+import com.example.pointme.models.Booking;
+import com.example.pointme.models.Event;
+import com.example.pointme.fragments.CategoriesFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import static com.example.pointme.activities.MainActivity.PROFILE_INFO;
 
 
-public class ServiceProviderMainActivity extends AppCompatActivity {
+public class ServiceProviderMainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private static final String TAG = ServiceProviderMainActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    //    private DatabaseReference mDatabase;
+    private BottomNavigationView bottomNavigationView;
+    public Toolbar toolbar;
+    private Event mEvent;
+    private Appointment mAppointment;
+    private Booking mBooking;
+
+    private ArrayList<String> mDates;
+    private ConnectionLiveData connectionLiveData;
+
+    private CardView noInternetView;
+    private FrameLayout container;
+    public BottomAppBar bottomAppBar;
+    private ProviderCalendarView providersCalendarFragment;
+
+    ServiceProvider provider;
+    /* Bundle Tags */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_main);
-
-        toolbar = findViewById(R.id.toolbar);
+        noInternetView = findViewById(R.id.noInternetCV);
+        container = findViewById(R.id.frame_container);
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+        toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
-
-        viewPager = findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+         provider = new ServiceProvider("test@test.com", "Lina Essam", "01225189191",
+                null, "https://firebasestorage.googleapis.com/v0/b/pointme-dbd0b.appspot.com/o/Users%2F7SePfePNkWbTVCVgG0NoXtl9IKI3.jpg?alt=media&token=6b731b9e-3c71-4bb6-9db1-58bbdbb815f2"
+        ,null, null,"Yoga", 3,4, 2, 5,10 );
+        providersCalendarFragment = new ProviderCalendarView();
+        loadFragment(providersCalendarFragment);
+        connectionLiveData = new ConnectionLiveData(this);
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ScheduleFragment(), "Schedule");
-        adapter.addFragment(new NewEventFragment(), "Events");
-        adapter.addFragment(new ProvidersEventsFragment(), "Stats");
-        viewPager.setAdapter(adapter);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        connectionLiveData.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                Log.d(TAG, "Connection changed " + aBoolean);
+                if (!aBoolean) {
+                    noInternetView.setVisibility(View.VISIBLE);
+                    container.setVisibility(View.GONE);
+                } else {
+                    noInternetView.setVisibility(View.GONE);
+                    container.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
+//
+    public void loadFragment(Fragment fragment) {
+        // load fragment
+        Log.d(TAG, "loading fragment");
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(PROFILE_INFO, provider);
+        fragment.setArguments(bundle);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+//
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.booking:
+                Log.i(TAG, "Loading favorites fragment");
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
+                loadFragment(providersCalendarFragment);
+                return true;
+            case R.id.action_needed:
+                Log.i(TAG, "Loading booking fragment");
+                MyBookingFragment bookingFragment = new MyBookingFragment();
+                loadFragment(bookingFragment);
+                return true;
+            case R.id.profile:
+                Log.i(TAG, "Loading categories fragment");
+                ProvidersProfileFragment profileFragment = new ProvidersProfileFragment();
+                loadFragment(profileFragment);
+                return true;
+            case R.id.more:
+                Log.i(TAG, "Loading settings fragment");
+                SettingsFragment settingsFragment = new SettingsFragment();
+                loadFragment(settingsFragment);
+                return true;
         }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/pointme/fragments/ProvidersProfileFragment.java
@@ -0,0 +1,260 @@
+package com.example.pointme.fragments;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.bumptech.glide.request.RequestOptions;
+import com.example.pointme.R;
+import com.example.pointme.adapters.ProfileAdapter;
+import com.example.pointme.models.ServiceProvider;
+import com.example.pointme.utils.GlideApp;
+import com.example.pointme.utils.SharedPreference;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static com.example.pointme.activities.MainActivity.PROFILE_INFO;
+import static com.example.pointme.activities.MainActivity.PROFILE_UID;
+
+public class ProvidersProfileFragment extends Fragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    private String TAG = ProfileFragment.class.getSimpleName();
+    private ProfileAdapter profileAdapter;
+    private String name;
+    private ServiceProvider profileInfo;
+
+    private String phoneNumber = "tel:";
+    private String instagramLink;
+
+    private SharedPreference sharedPreference;
+
+    /*Views*/
+    private CircleImageView profileImage;
+    private LinearLayout phoneLinearLayout;
+    private LinearLayout instagramLinearLayout;
+    private RatingBar mRatingBar;
+    private LinearLayout eventLinearLayout;
+    private LinearLayout reviewsLinearLayout;
+    private TextView eventsTv;
+    private TextView reviewsTv;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            profileInfo = getArguments().getParcelable(PROFILE_INFO);
+        }
+        sharedPreference = new SharedPreference();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().setTitle("My Profile");
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+
+        View mRootview = inflater.inflate(R.layout.fragment_profile, parent, false);
+        // Defines the xml file for the fragment
+
+        /* Initalizing views */
+        profileImage = mRootview.findViewById(R.id.ivProfile);
+        TextView nameView = mRootview.findViewById(R.id.tvName);
+        phoneLinearLayout = mRootview.findViewById(R.id.callLayout);
+        instagramLinearLayout = mRootview.findViewById(R.id.instagramLayout);
+        mRatingBar = mRootview.findViewById(R.id.rating);
+        TextView numOfReviews = mRootview.findViewById(R.id.numReviews);
+        eventsTv = mRootview.findViewById(R.id.eventTv);
+        reviewsTv = mRootview.findViewById(R.id.reviewsTv);
+        eventLinearLayout = mRootview.findViewById(R.id.eventsLinearLayout);
+        reviewsLinearLayout = mRootview.findViewById(R.id.reviewsLinearLayout);
+
+        eventLinearLayout.setOnClickListener(this);
+        eventLinearLayout.setClickable(false);
+
+        reviewsLinearLayout.setOnClickListener(this);
+        reviewsLinearLayout.setClickable(true);
+
+
+        phoneLinearLayout.setOnClickListener(this);
+        phoneLinearLayout.setClickable(false);
+
+        instagramLinearLayout.setOnClickListener(this);
+        instagramLinearLayout.setClickable(false);
+
+        GlideApp.with(this)
+                .load(profileInfo.getImage())
+                .apply(new RequestOptions()
+                        .placeholder(R.drawable.no_image)
+                        .error(R.drawable.no_image)
+                        .fitCenter())
+                .into(profileImage);
+
+        nameView.setText(profileInfo.getName());
+        mRatingBar.setRating(profileInfo.getAvgRating());
+        numOfReviews.setText("(" + profileInfo.getNumReviews() + ")");
+        addLinks();
+
+        loadEventFragment();
+
+        return mRootview;
+    }
+
+    private void addLinks() {
+
+        if (profileInfo.getTel() != null) {
+            phoneNumber += profileInfo.getTel();
+            phoneLinearLayout.setClickable(true);
+        }
+
+        if (profileInfo.getIg() != null) {
+            instagramLink = profileInfo.getIg();
+            instagramLinearLayout.setClickable(true);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.callLayout:
+                callServiceProvider();
+                break;
+            case R.id.instagramLayout:
+                openInstagram();
+                break;
+            case R.id.eventsLinearLayout:
+                reviewsLinearLayout.setBackgroundResource(0);
+                eventLinearLayout.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.capsule_white_background));
+                eventsTv.setTextColor(Color.BLACK);
+                reviewsTv.setTextColor(Color.WHITE);
+                loadEventFragment();
+                eventLinearLayout.setClickable(false);
+                reviewsLinearLayout.setClickable(true);
+                break;
+            case R.id.reviewsLinearLayout:
+                eventLinearLayout.setBackgroundResource(0);
+                reviewsLinearLayout.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.capsule_white_background));
+                reviewsTv.setTextColor(Color.BLACK);
+                eventsTv.setTextColor(Color.WHITE);
+                loadReviewsFragment();
+                reviewsLinearLayout.setClickable(false);
+                eventLinearLayout.setClickable(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        final ScaleAnimation scaleAnimation = new ScaleAnimation(0.7f, 1.0f, 0.7f, 1.0f, Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f);
+        scaleAnimation.setDuration(500);
+        BounceInterpolator bounceInterpolator = new BounceInterpolator();
+        scaleAnimation.setInterpolator(bounceInterpolator);
+
+        //animation
+        compoundButton.startAnimation(scaleAnimation);
+
+        if (isChecked) {
+            sharedPreference.addFavorite(getActivity(), profileInfo);
+            Toast.makeText(getActivity(), "Added to favorites", Toast.LENGTH_LONG).show();
+        } else {
+            sharedPreference.removeFavorite(getActivity(), profileInfo);
+            Toast.makeText(getActivity(), "Removed from favorites", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void callServiceProvider() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNumber));
+        startActivity(intent);
+    }
+
+    private void openInstagram() {
+        if (!instagramLink.isEmpty()) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://instagram.com/_u/" + instagramLink));
+                intent.setPackage("com.instagram.android");
+                startActivity(intent);
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.instagram.com/" + instagramLink)));
+            }
+        } else {
+            Toast.makeText(getActivity(), "User doesn't have instagram account", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void loadEventFragment() {
+        Log.d(TAG, "loading fragment");
+        EventsFragment fragment = new EventsFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(PROFILE_INFO, profileInfo);
+        fragment.setArguments(bundle);
+        FragmentTransaction transaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slide_to_left, R.anim.slide_from_left, R.anim.slide_to_right);
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+    }
+
+    private void loadReviewsFragment() {
+        Log.d(TAG, "loading fragment");
+        ReviewFragment fragment = new ReviewFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(PROFILE_UID, profileInfo.getuID());
+        fragment.setArguments(bundle);
+        FragmentTransaction transaction = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.slide_from_left, R.anim.slide_to_right, R.anim.slide_from_right, R.anim.slide_to_left);
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+    }
+
+
+
+}
